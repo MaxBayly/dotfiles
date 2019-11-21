69436847d616bffb9a6288dfa49bd196a7ea3e25
MaxBayly <maxbayly@gmail.com>
2019-11-22 00:46:15 +1100

    add comments and load directory for skyfield

--- sunup.py
+++ sunup.py
@@ -1,24 +1,31 @@
+"""Exits to system 1 if sun is up, 0 otherwise"""
 import sys
 from skyfield import api
 from skyfield import almanac
 from datetime import timedelta
-from skyfield.api import utc
+from skyfield.api import utc, Loader
 import datetime
 
-ts = api.load.timescale()
-e = api.load('de421.bsp')
+# load in data directory to avoid redownloading
+loader = Loader('~/skyfield_data')
+ts = loader.timescale()
+e = loader('de421.bsp')
 
+# set current location (melbourne does not appear in the default list)
 melbourne = api.Topos('37.951910 S', '145.152080 E')
-
+# get current time in UTC format
 now = datetime.datetime.utcnow()
 now = now.replace(tzinfo=utc)
-
+# set the interval for now and 24 hours from now
 t0 = ts.utc(now)
 t1 = ts.utc(now + timedelta(hours=24))
 
+# find the times and types of event (sunrise/sunset)
 t, y = almanac.find_discrete(t0, t1, almanac.sunrise_sunset(e, melbourne))
+# print(t.utc_iso())
+# print(y)
 
-
+# if the first element is True, which means a sunrise, the sun must be down (and vice versa)
 if (y[0]):
 	sys.exit("0")
 else:
--- thief.py
+++ thief.py
@@ -3,10 +3,13 @@ import matplotlib.pyplot as plt
 import sys
 imagepath = sys.argv[1]
 
+# instantiate colourthief
 color_thief = cf(imagepath)
 pltcolours = []
 hex = []
+# get palette
 palette = color_thief.get_palette(color_count=6)
+# convert to hex 
 for colour in palette:
 	hex.append('#%02x%02x%02x' % colour)
 	pltcolours.append([colour])
