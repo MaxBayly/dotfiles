75f7c337db8255e64612759b2390f92659cb505f
MaxBayly <maxbayly@gmail.com>
2019-12-31 13:33:44 +1100

    removed colour magic number

--- thief.py
+++ thief.py
@@ -2,13 +2,14 @@ from colorthief import ColorThief as cf
 import matplotlib.pyplot as plt
 import sys
 imagepath = sys.argv[1]
+COLOUR_COUNT=6
 
 # instantiate colourthief
 color_thief = cf(imagepath)
 pltcolours = []
 hex = []
 # get palette
-palette = color_thief.get_palette(color_count=6)
+palette = color_thief.get_palette(color_count=COLOUR_COUNT)
 # convert to hex 
 for colour in palette:
 	hex.append('#%02x%02x%02x' % colour)
