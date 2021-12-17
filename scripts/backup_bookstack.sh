0eb640ef43e5b42f6195df1a37f4cda5b5e3602a
Max Bayly <maxbayly@gmail.com>
2021-12-18 10:12:39 +1100

    update script

--- scripts/backup_bookstack.sh
+++ scripts/backup_bookstack.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh rroche@192.168.0.79 ~/bookstack_backup.sh;scp "rroche@192.168.0.79:~/bookstack-files_*" ~/Downloads/;ssh rroche@192.168.0.79 rm "~/bookstack-files_**"
+ssh rroche@192.168.0.79 ~/bookstack_backup.sh;scp "rroche@192.168.0.79:~/bookstack-backup_*" ~/Downloads/;ssh rroche@192.168.0.79 rm "~/bookstack-backup_*"
